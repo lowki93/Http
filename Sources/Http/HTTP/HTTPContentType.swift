90ba15225d9f34ac079bc8ead4f93912bf8c64d0
lowki93 <budain.kevin@yahoo.Fr>
2022-01-12 01:36:00 +0100

    add HTTPHeader and HTTPContentType

--- Sources/Http/HTTP/HTTPContentType.swift
+++ Sources/Http/HTTP/HTTPContentType.swift
@@ -2,17 +2,20 @@ import Foundation
 
 /// A struct representing a header content type value
 public struct HTTPContentType: Hashable, ExpressibleByStringLiteral {
-    let value: String
+  let value: String
 
-    public init(value: String) {
-      self.value = value
-    }
+  public init(value: String) {
+    self.value = value
+  }
 
-    public init(stringLiteral value: StringLiteralType) {
-      self.value = value
-    }
+  public init(stringLiteral value: StringLiteralType) {
+    self.value = value
+  }
 }
 
 extension HTTPContentType {
-    public static let json: Self = "application/json"
+  public static let json: Self = "application/json"
+  public static func multipart(boundary: String) -> Self {
+    .init(stringLiteral: "multipart/form-data; boundary=\(boundary)")
+  }
 }
--- Sources/Http/HTTP/HTTPHeader.swift
+++ Sources/Http/HTTP/HTTPHeader.swift
@@ -13,4 +13,11 @@ extension HTTPHeader {
   public static let accept: Self = "Accept"
   public static let authentication: Self = "Authentication"
   public static let contentType: Self = "Content-Type"
+  public static var contentDisposition: Self = "Content-Disposition"
+  public static func contentDisposition(disposition: String) -> String {
+    [Self.contentDisposition.key, disposition].joined(separator: ": ")
+  }
+  public static func contentType(_ disposition: String) -> String {
+    [Self.contentType.key, disposition].joined(separator: ": ")
+  }
 }
